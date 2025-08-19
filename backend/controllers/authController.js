46e195169dda30982ab0fc6d86cab2b8cb0cd8d1
OmParkash-27 <103979931+OmParkash-27@users.noreply.github.com>
2025-08-19 16:26:46 +0530

    change in login register cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -31,8 +31,8 @@
       // Send token via cookie
       res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        secure: true,
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000
       });
 
@@ -59,8 +59,8 @@
       // Set cookie
       res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        secure: true,
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000
       });
 
